3d1a0c7fc3f76e138291673cd136c9c37ac8a671
Abel Domínguez Morejón <temausdomo@gmail.com>
2023-02-17 14:34:57 +0100

    Añadir llamada CRUD con componentes Boton y Articulo

--- src/Articulos.svelte
+++ src/Articulos.svelte
@@ -1,20 +1,24 @@
 <script>
+    import { getContext } from "svelte";
+    import { data } from "./store.js";
     import { onMount } from "svelte";
     import Buscar from "./Buscar.svelte";
+    import Articulo from "./Articulo.svelte";
+    import Boton from "./Boton.svelte";
 
-    const URL = "https://tiendabackend.fly.dev/api/articulos";
-    let data = [];
+    const URL = getContext("URL");
     let datosFiltrados = [];
     let patron = "";
+    let articuloInsertar = {};
 
     let getArticulos = async () => {
-        const response = await fetch(URL);
-        data = await response.json();
+        const response = await fetch(URL.articulos);
+        $data = await response.json();
     };
 
     onMount(getArticulos);
 
-    $: datosFiltrados = data.filter((articulo) =>
+    $: datosFiltrados = $data.filter((articulo) =>
         RegExp(patron, "i").test(articulo.nombre)
     );
 </script>
@@ -23,8 +27,18 @@
 
 <hr />
 
+<Articulo bind:articulo={articuloInsertar}>
+    <Boton documento={articuloInsertar} />
+</Articulo>
+
+<br /><br />
+
 {#each datosFiltrados as articulo}
-    {articulo.nombre} - {articulo.precio} €<br />
+    <Articulo bind:articulo>
+        <Boton tipo="modificar" documento={articulo} />
+        <Boton tipo="eliminar" documento={articulo} />
+    </Articulo>
+    <br /><br />
 {/each}
 
 <hr />
